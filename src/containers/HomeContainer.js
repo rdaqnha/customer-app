a73931d2e7de61ba6317ab841ae008d84455a95f
Raul Dacuña <rdanqha@gmail.com>
2019-08-01 11:04:13 +0200

    quit console

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import AppFrame from '../components/AppFrame';
 import CustomerActions from '../components/CustomerActions';
 
 class HomeContainer extends Component {
 
     handleOnClick = () => {
-        console.log("HandleOnClick");
+        this.props.history.push('/customers');
     } 
 
     render() {
