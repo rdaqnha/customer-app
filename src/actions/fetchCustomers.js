b0a84f00ef81ef16e3fb3f499dae4b6904006dd1
Raul Dacuña <raul.dacuna@datiobbva.com>
2019-08-02 14:32:42 +0200

    Change fetch with redux-promise and json-server

--- src/actions/fetchCustomers.js
+++ src/actions/fetchCustomers.js
@@ -1,22 +1,6 @@
 import { FETCH_CUSTOMERS } from './../constants/index';
-import { createAction } from 'redux-actions';
+import { createAction } from 'redux-actions'; 
+import { apiGet } from '../api';
+import { urlCustomers } from './../api/urls';
 
-const customers = [
-    {
-        "dni": "343423432",
-        "name": "Manolo García Pio",
-        "age": 23
-    },
-    {
-        "dni": "344233432",
-        "name": "Paco Nuñez",
-        "age": 35
-    },
-    {
-        "dni": "5343423432",
-        "name": "Jose Blanco",
-        "age": 56
-    }
-];
-
-export const fetchCustomers = createAction(FETCH_CUSTOMERS, () => customers);
+export const fetchCustomers = createAction(FETCH_CUSTOMERS, apiGet(urlCustomers));
---
+++ src/api/urls.js
@@ -0,0 +1,3 @@
+const urlBase = 'http://localhost:3002/';
+
+export const urlCustomers = `${urlBase}customers`;
